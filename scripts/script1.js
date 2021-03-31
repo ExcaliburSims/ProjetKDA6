f4bf4e68887abedb5d1cd9921d759052145dc377
sim's zilu <ximeonzilu@gmail.com>
2021-03-31 15:10:53 +0100

    ajout des fichiers constituants

---
+++ scripts/script1.js
@@ -0,0 +1,7 @@
+var dit=document.querySelector('.stitr1');
+var bouton=document.querySelector('#btn1');
+bouton.addEventListener('click',maFonction);
+
+function maFonction() {
+    alert('salut');
+}
