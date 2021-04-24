f95a0419f8ead28c60153a1e38c8136328965148
sim's zilu <ximeonzilu@gmail.com>
2021-04-24 07:36:49 +0100

    ajout des fichiers constituants

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,7 +1,3 @@
 var dit=document.querySelector('.firsttile');
 var bouton=document.querySelector('#buttoncreer');
-bouton.addEventListener('click',maFonction);
-
-function maFonction() {
-    alert('salut');
-}
+bouton.addEventListener('click',()=>alert('salut'));
